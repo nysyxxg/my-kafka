c886e5fa74be1e9c94dd333f7ad2b5abccb4a0e3
xuxg <xuxiaoguang@mininglamp.com>
2021-01-07 23:06:00 +0800

    kafka源码重构

---
+++ kafka-0821/kafka-0821-examples/src/main/java/kafka/demo/ConsumerGroupExample.java
@@ -0,0 +1,61 @@
+package kafka.demo;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.examples.KafkaProperties;
+import kafka.javaapi.consumer.ConsumerConnector;
+   
+public class ConsumerGroupExample {  
+	
+    private final ConsumerConnector consumer;  
+    private final String topic;  
+    private  ExecutorService executor;  
+   
+    public ConsumerGroupExample(String a_zookeeper, String a_groupId, String a_topic) {  
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(  
+                createConsumerConfig(a_zookeeper, a_groupId));  
+        this.topic = a_topic;  
+    }  
+   
+    public void shutdown() {  
+        if (consumer != null) consumer.shutdown();  
+        if (executor != null) executor.shutdown();  
+    }  
+   
+    public void run(int a_numThreads) {  
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();  
+        topicCountMap.put(topic, new Integer(a_numThreads));
+        
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);  
+        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);  
+   
+        // 创建一个线程池
+        executor = Executors.newFixedThreadPool(a_numThreads);  
+   
+        // 开始消费消息  
+        int threadNumber = 0;  
+        for (final KafkaStream<byte[], byte[]> stream : streams) {  
+        	// 提交执行每个任务
+            executor.submit(new ConsumerService(stream, threadNumber));  
+            threadNumber++;  
+        }  
+    }  
+   
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {  
+        Properties props = new Properties();  
+        props.put("zookeeper.connect", KafkaProperties.ZK_CONNECT);  
+        props.put("group.id", KafkaProperties.GroupId);  
+        props.put("zookeeper.session.timeout.ms", "60000");  
+        props.put("zookeeper.sync.time.ms", "2000");  
+        props.put("auto.commit.interval.ms", "1000");  
+   
+        return new ConsumerConfig(props);  
+    }  
+   
+}  
---
+++ kafka-0821/kafka-0821-examples/src/main/java/kafka/demo/ConsumerService.java
@@ -0,0 +1,30 @@
+package kafka.demo;
+
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+
+
+/**
+ * 多线程并行消费topic
+ */
+public class ConsumerService implements Runnable {
+    private KafkaStream<byte[], byte[]> m_stream;
+    private int m_threadNumber;
+    
+    
+    public ConsumerService(KafkaStream<byte[], byte[]> a_stream, int a_threadNumber) {
+        this.m_threadNumber = a_threadNumber;
+        this.m_stream = a_stream;
+    }
+    
+    public void run() {
+        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
+        byte[] by = null;
+        while (it.hasNext()) {
+            by = it.next().message();
+            String value = new String(by);
+            System.out.println("Thread " + m_threadNumber + ": " + value);
+            System.out.println(Thread.currentThread().getName() + " ....Shutting down Thread: " + m_threadNumber);
+        }
+    }
+}  
